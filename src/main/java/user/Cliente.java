5e46ff637df81aa68048dd729f6004b11a254301
DarioDeMaio <demaio.dario95@gmail.com>
2023-01-27 08:22:14 +0100

    Aggiunto metodo in UserManager. Effettuata suddivisione pacchetti.

--- src/main/java/user/Cliente.java
+++
@@ -1,9 +0,0 @@
-package user;
-
-public class Cliente extends Utente{
-
-    public Cliente(int telefono, String nome, String cognome, String email, String password) {
-        super(telefono, nome, cognome, email, password);
-    }
-
-}
