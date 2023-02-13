ecb0c18e69eac4c07279a01781a775b37f0bffb4
DarioDeMaio <115263219+DarioDeMaio@users.noreply.github.com>
2023-02-13 19:29:29 +0100

    Delete src/main directory

--- src/main/java/user/bean/Cliente.java
+++
@@ -1,9 +0,0 @@
-package user.bean;
-
-public class Cliente extends Utente{
-
-    public Cliente(String telefono, String nome, String cognome, String email, String password) {
-        super(telefono, nome, cognome, email, password);
-    }
-
-}
